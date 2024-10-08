c013a3658d37d0c073a3687517c5731073b67432
Sebastián García <johancs.mm@gmail.com>
2024-10-07 22:22:00 -0500

    feat: implement an endpoint example (#1)

--- src/api/users.rs
+++ src/api/users.rs
@@ -1,5 +1,14 @@
+use axum::response::{self, IntoResponse};
 use axum::routing::post;
-use axum::Router;
+use axum::{Extension, Json, Router};
+use serde::{Deserialize, Serialize};
+
+use crate::db::{CreateUser, Database, User};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct ServerResponse {
+  users: Vec<User>,
+}
 
 pub fn users() -> Router {
   Router::new().nest(
@@ -10,8 +19,16 @@ pub fn users() -> Router {
   )
 }
 
-async fn register() -> &'static str {
-  "Register user"
+async fn register(
+  Extension(db): Extension<Database>,
+  Json(create_user): Json<CreateUser>,
+) -> impl IntoResponse {
+  let users = db.get_users().await.unwrap();
+
+  let server_response = ServerResponse { users };
+
+  println!("{:?}", create_user);
+  response::Json(server_response).into_response()
 }
 
 async fn login() -> &'static str {
